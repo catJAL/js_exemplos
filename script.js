672388d64532fa89bc8d0cd013aed89655a7af56
Arthur Brito <aline.nolasco.brito@gmail.com>
2023-05-19 13:53:52 -0300

    COMIT

---
+++ script.js
@@ -0,0 +1,10 @@
+// var valor = confirm("Vamos juntos aprender a programar JavaScript?");
+// if (valor == true) {
+//     alert("Parabéns, vamos lá!");
+// } else {
+//     alert("Opção errada, aperte f5 para recarregar a página");
+// }
+
+
+var email = prompt("Digite seu e-mail", "");
+alert("O e-mail " + email + " foi coletado com sucesso.");
